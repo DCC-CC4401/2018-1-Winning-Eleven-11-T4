c7ad5b25ad90c226eb3ee6d20885969c438502e0
Seba Arriola <sarriola@ing.uchile.cl>
2018-08-17 01:39:22 -0300

    Requisito 61 terminado

--- cc4401Inventory/adminApp/views.py
+++ cc4401Inventory/adminApp/views.py
@@ -9,6 +9,10 @@ from datetime import datetime, timedelta, date
 import pytz
 from django.utils.timezone import localtime
 from django.contrib import messages
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
+import os
+from django.conf import settings
 
 @login_required
 def user_panel(request):
@@ -122,8 +126,10 @@ def add_new_item(request):
     if request.method == 'POST':
         name = request.POST['name']
         description = request.POST['description']
-        image = request.POST['image']
-        new_item = Article(name=name, description=description, image = image, state='D')
+        image = request.FILES['image']
+        tmp_file = os.path.join('mainApp/static/mainApp/img/items', image.name)
+        path = default_storage.save(tmp_file, ContentFile(image.read()))
+        new_item = Article(name=name, description=description, image = path, state='D')
         new_item.save()
         messages.success(request, 'Articulo agregado correctamente.')
     return redirect('/admin/items-panel/')
